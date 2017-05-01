8809703d5e1744a48e3def7b9d5c38ce4c0aa068
Kirill Ponomarenko <kir-rik-90@yandex.ru>
2017-05-01 22:32:37 +0400

    some stuff

--- js/slider.js
+++ js/slider.js
@@ -3,13 +3,40 @@ const sliderJsModule = (function(){
         createSlider: createSlider
     }
     /**
-     * creates slider in container with tragetId Id from content of container with sourceId Id 
+     * creates slider
      * @function
-     * @param {*} tragetId
-     * @param {*} sourceId
+     * @param {string} sourceId - slider container id
+     * @param {Object} config - configuration object
+     * @param {number} config.width - Set width of the slideshow.
+     * @param {number} config.heigth - Set height of the slideshow.
+     * @param {number} config.start - Set the first slide in the slideshow
      */
-    function createSlider(tragetId, sourceId){
-        console.log('slide this!', tragetId, containerId);
+    function createSlider(sourceId, config){
+
+        var container = document.getElementById(sourceId);
+        var urls = getImgsUrlFromContainer(container);
+        // container.style.overflow = 'hidden';
+        // container.style.display = 'block';
+
+        var innerContainer  = document.createElement('div');
+        innerContainer.style.overflow = 'hidden';
+        innerContainer.style.position= 'relative';
+        innerContainer.style.width = (config.width || 1024)+'px;'
+        innerContainer.style.heigth = (config.heigth || 768)+'px;'
+
+        container.appendChild(innerContainer);
+
     }
+
+    // function getImgsUrlFromContainer(elem){
+    //     var urls = [];
+    //     for (var i = 0; i < elem.childNodes.length; i++) {
+    //         var node = elem.childNodes[i];
+    //         var srcVal = elem.getAttribute("src");
+    //         if (node.tagName == 'IMG' && srcVal){
+    //             urls.push(srcVal);
+    //         }
+    //     }
+    // }
 
 })();
