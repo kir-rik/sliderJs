d0635c0f676d13801e57d04d2d2ce1a7bb508485
Kirill Ponomarenko <kir-rik-90@yandex.ru>
2017-05-01 15:50:09 +0400

    folders and files

---
+++ js/slider.js
@@ -0,0 +1,14 @@
+const sliderJsModule = (function(){
+    return {
+        createSlider: createSlider
+    }
+    /**
+     * creates slider from content of container with containerId Id
+     * @function
+     * @param {*} containerId
+     */
+    function createSlider(containerId){
+
+    }
+
+})();
